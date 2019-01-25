06446dbb0214129d6362205a14286b0c6704cd07
Nuno Rafael Rocha <nunorafaelrocha@gmail.com>
2019-01-25 23:37:36 +0100

    Update prompt configuration to use Powerlevel9k

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,72 +1,18 @@
-autoload colors && colors
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
-if (( $+commands[git] ))
-then
-  git="$commands[git]"
-else
-  git="/usr/bin/git"
-fi
+# Customise the Powerlevel9k prompts
+export POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(custom_flame context dir vcs)
+export POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=()
 
-git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
+# Add the custom flame prompt segment
+export POWERLEVEL9K_CUSTOM_FLAME="echo -n $'\uF490'"
+export POWERLEVEL9K_CUSTOM_FLAME_FOREGROUND="black"
+export POWERLEVEL9K_CUSTOM_FLAME_BACKGROUND="white"
 
-git_dirty() {
-  if $(! $git status -s &> /dev/null)
-  then
-    echo ""
-  else
-    if [[ $($git status --porcelain) == "" ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    fi
-  fi
-}
+# Add custom format and colors to context segment.
+export POWERLEVEL9K_CONTEXT_TEMPLATE="%n"
+export POWERLEVEL9K_CONTEXT_DEFAULT_FOREGROUND="white"
+export POWERLEVEL9K_CONTEXT_DEFAULT_BACKGROUND="cyan"
 
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
-}
-
-# This assumes that you always have an origin named `origin`, and that you only
-# care about one specific origin. If this is not the case, you might want to use
-# `$git cherry -v @{upstream}` instead.
-need_push () {
-  if [ $($git rev-parse --is-inside-work-tree 2>/dev/null) ]
-  then
-    number=$($git cherry -v origin/$(git symbolic-ref --short HEAD) 2>/dev/null | wc -l | bc)
-
-    if [[ $number == 0 ]]
-    then
-      echo " "
-    else
-      echo " with %{$fg_bold[magenta]%}$number unpushed%{$reset_color%}"
-    fi
-  fi
-}
-
-directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
-}
-
-battery_status() {
-  if [[ $(sysctl -n hw.model) == *"Book"* ]]
-  then
-    $ZSH/bin/battery-status
-  fi
-}
-
-export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
-}
-
-precmd() {
-  title "zsh" "%m" "%55<...<%~"
-  set_prompt
-}
+export POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+export POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=""
+export POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="\uf054 "
