348674711b14ebeee854a496e4424329f4b96a15
Nuno Rafael Rocha <nunorafaelrocha@users.noreply.github.com>
2020-09-24 20:42:18 +0100

    chore: spotlight application list

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -492,28 +492,28 @@ sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Vol
 # 	MENU_WEBSEARCH             (send search queries to Apple)
 # 	MENU_OTHER
 defaults write com.apple.spotlight orderedItems -array \
-	'{"enabled" = 1;"name" = "APPLICATIONS";}' \
-	'{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
-	'{"enabled" = 1;"name" = "DIRECTORIES";}' \
-	'{"enabled" = 1;"name" = "MENU_CONVERSION";}' \
-	'{"enabled" = 1;"name" = "PDF";}' \
-	'{"enabled" = 0;"name" = "DOCUMENTS";}' \
-	'{"enabled" = 0;"name" = "FONTS";}' \
-	'{"enabled" = 0;"name" = "MESSAGES";}' \
-	'{"enabled" = 0;"name" = "CONTACT";}' \
-	'{"enabled" = 0;"name" = "EVENT_TODO";}' \
-	'{"enabled" = 0;"name" = "IMAGES";}' \
-	'{"enabled" = 0;"name" = "BOOKMARKS";}' \
-	'{"enabled" = 0;"name" = "MUSIC";}' \
-	'{"enabled" = 0;"name" = "MOVIES";}' \
-	'{"enabled" = 0;"name" = "PRESENTATIONS";}' \
-	'{"enabled" = 0;"name" = "SPREADSHEETS";}' \
-	'{"enabled" = 0;"name" = "SOURCE";}' \
-	'{"enabled" = 0;"name" = "MENU_DEFINITION";}' \
-	'{"enabled" = 0;"name" = "MENU_OTHER";}' \
-	'{"enabled" = 0;"name" = "MENU_EXPRESSION";}' \
-	'{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
-	'{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
+		'{"enabled" = 1;"name" = "APPLICATIONS";}' \
+		'{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
+		'{"enabled" = 1;"name" = "DIRECTORIES";}' \
+		'{"enabled" = 1;"name" = "MENU_CONVERSION";}' \
+		'{"enabled" = 0;"name" = "PDF";}' \
+		'{"enabled" = 0;"name" = "DOCUMENTS";}' \
+		'{"enabled" = 0;"name" = "FONTS";}' \
+		'{"enabled" = 0;"name" = "MESSAGES";}' \
+		'{"enabled" = 0;"name" = "CONTACT";}' \
+		'{"enabled" = 0;"name" = "EVENT_TODO";}' \
+		'{"enabled" = 0;"name" = "IMAGES";}' \
+		'{"enabled" = 0;"name" = "BOOKMARKS";}' \
+		'{"enabled" = 0;"name" = "MUSIC";}' \
+		'{"enabled" = 0;"name" = "MOVIES";}' \
+		'{"enabled" = 0;"name" = "PRESENTATIONS";}' \
+		'{"enabled" = 0;"name" = "SPREADSHEETS";}' \
+		'{"enabled" = 0;"name" = "SOURCE";}' \
+		'{"enabled" = 0;"name" = "MENU_DEFINITION";}' \
+		'{"enabled" = 0;"name" = "MENU_OTHER";}' \
+		'{"enabled" = 1;"name" = "MENU_EXPRESSION";}' \
+		'{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTION";}'
+
 # Load new settings before rebuilding the index
 killall mds > /dev/null 2>&1
 # Make sure indexing is enabled for the main volume
