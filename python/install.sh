8628e8ed75ff01420ed19c94e92eab806d036e4f
Nuno Rafael Rocha <nunorafaelrocha@users.noreply.github.com>
2020-10-04 16:24:51 +0100

    chore: improve python install

---
+++ python/install.sh
@@ -0,0 +1,4 @@
+# pyenv
+if test ! $(which pyenv); then
+  curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+fi
---
+++ python/path.zsh
@@ -0,0 +1,1 @@
+export PATH="$HOME/.pyenv/bin:$PATH"
