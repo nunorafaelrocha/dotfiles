c472c6a15d0662c85336052e98af764634ac7f11
Nuno Rafael Rocha <nunorafaelrocha@users.noreply.github.com>
2020-10-05 20:44:53 +0100

    feat: docker aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,2 +1,26 @@
-alias d='docker $*'
-alias d-c='docker-compose $*'
+dkr() {
+  case "$1" in
+      # kill all running containers
+      "kill")
+        docker kill $(docker ps -q)
+        ;;
+      # remove all containers
+      "rmc")
+        docker rm $(docker ps -a -q)
+        ;;
+      # remove all images
+      "rmi")
+        docker rmi $(docker images -q)
+        ;;
+      # reset
+      "reset")
+        dkr kill
+        docker network prune -f
+        dkr rmc
+        dkr rmi
+        ;;
+      *)
+        echo "Invalid option $1"
+        ;;
+    esac
+}
