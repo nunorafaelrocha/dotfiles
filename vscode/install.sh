4c0b3248a182214f0f2c090428475348ef451bda
Nuno Rafael Rocha <nunorafaelrocha@users.noreply.github.com>
2020-11-17 16:40:04 +0100

    fix: vscode user config folder

--- vscode/install.sh
+++ vscode/install.sh
@@ -2,6 +2,7 @@
 
 if [ `uname` == "Darwin" ]; then
   rm -rf $HOME/Library/Application\ Support/Code/User
+  mkdir -p $HOME/Library/Application\ Support/Code
   ln -s $DOTFILES/vscode/User $HOME/Library/Application\ Support/Code/User
 fi
 
