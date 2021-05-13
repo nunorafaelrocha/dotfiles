2ac0dd8b7b33473e03f23fb2c716bdc1b146b62c
Nuno Rafael Rocha <nunorafaelrocha@users.noreply.github.com>
2021-05-13 19:16:45 +0200

    feat: replace gls by exa for ls alias

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,12 +1,11 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-if $(gls &>/dev/null)
+
+# exa overides for ls
+if $(exa &>/dev/null)
 then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
+  alias ls="exa"
+  alias l="exa -l"
+  alias ll="exa -la"
+  alias la='exa -a'
 fi
 
 # Add macos alias
