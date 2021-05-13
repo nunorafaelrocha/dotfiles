bd979ef0a89dac2db8e6e3607707a3802efac183
Nuno Rafael Rocha <nunorafaelrocha@users.noreply.github.com>
2021-05-13 18:00:05 +0100

    feat: set computer name

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -24,10 +24,10 @@ fi
 ###############################################################################
 
 # Set computer name (as done via System Preferences → Sharing)
-#sudo scutil --set ComputerName "0x6D746873"
-#sudo scutil --set HostName "0x6D746873"
-#sudo scutil --set LocalHostName "0x6D746873"
-#sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "0x6D746873"
+sudo scutil --set ComputerName "macnuno"
+sudo scutil --set HostName "macnuno"
+sudo scutil --set LocalHostName "macnuno"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "macnuno"
 
 # Save to disk (not to iCloud) by default
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
