dd9b009596d6b799b2a6a6d83f5803014d6f8aee
Nuno Rocha <1175149+nunorafaelrocha@users.noreply.github.com>
2025-05-10 22:13:51 +0200

    fix: zshrc

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -4,6 +4,10 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
 
+# Load and initialize the completion system
+autoload -Uz compinit
+compinit
+
 # Init fasd and cache init code
 fasd_cache="$HOME/.fasd-init-bash"
 
