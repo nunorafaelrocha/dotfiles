a2cebb56317b3b7de1e1d5d9cc60e6f142bafbb3
Nuno Rocha <1175149+nunorafaelrocha@users.noreply.github.com>
2025-06-09 16:50:28 +0200

    chore:remove vscode

--- vscode/install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-rm -rf $HOME/Library/Application\ Support/Code/User
-mkdir -p $HOME/Library/Application\ Support/Code
-ln -s $DOTFILES/vscode/User $HOME/Library/Application\ Support/Code/User
